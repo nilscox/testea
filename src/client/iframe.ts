98a3199044f6903112ea8c29e52417bc554a1bde
Nils <nils@epita.fr>
2020-12-28 19:47:54 +0100

    display the tests and the iframe side by side

--- src/client/iframe.ts
+++ src/client/iframe.ts
@@ -2,12 +2,6 @@ import { getQueriesForElement, wait } from '@testing-library/dom';
 
 let queries = getQueriesForElement(document.body);
 
-class PostMessageError extends Error {
-  constructor(public readonly message: any, public readonly data: any) {
-    super('IFrame.postMessage() failed');
-  }
-}
-
 export class IFrame {
   constructor(public readonly element: HTMLIFrameElement) {}
 
@@ -27,82 +21,24 @@ export class IFrame {
     return this.contentWindow?.location;
   }
 
-  async postMessage(message: any) {
-    return new Promise((resolve, reject) => {
-      if (!this.contentWindow || !this.location) {
-        throw new Error('IFrame: contentWindow is not set');
-      }
-
-      if (this.location.href === 'about:blank') {
-        throw new Error('IFrame: cannot postMessage when not in a page');
-      }
-
-      const commandId = Math.random().toString(36).slice(6);
-
-      const handleResponse = ({ data }: MessageEvent) => {
-        if (!data || !data.__test_runner_response__ || data.commandId !== commandId) {
-          return;
-        }
-
-        this.contentWindow?.removeEventListener('message', handleResponse);
-
-        if (data.success !== true) {
-          reject(new PostMessageError(message, data));
-        } else {
-          resolve(data);
-        }
-      };
-
-      this.contentWindow.addEventListener('message', handleResponse);
-      this.contentWindow.postMessage({ __test_runner_request__: true, commandId, ...message }, this.location.href);
-    });
-  }
-
-  async clearCookies(domain: string) {
+  async clearCookies() {
     const now = new Date().toUTCString();
 
     this.document?.cookie.split(';').forEach(c => {
       this.document!.cookie = c.replace(/^ +/, '').replace(/=.*/, `=;expires=${now};path=/`);
     });
-    // return this.postMessage(window.location.href, { type: 'clearCookies', target: 'background_script', domain });
   }
 
   async clearLocalStorage() {
-    return this.postMessage({ type: 'clearLocalStorage', target: 'content_script' });
+    return this.contentWindow?.localStorage.clear();
   }
 
   async getLocalStorageItem(key: string) {
-    return this.postMessage({ type: 'getLocalStorageItem', target: 'content_script', key });
+    return this.contentWindow?.localStorage.getItem(key);
   }
 
   async setLocalStorageItem(key: string, value: string) {
-    return this.postMessage({ type: 'setLocalStorageItem', target: 'content_script', key, value });
-  }
-
-  private async injectContentScript() {
-    const { document, body } = this;
-
-    if (!document || !body) {
-      throw new Error();
-    }
-
-    return new Promise<void>(resolve => {
-      const handleMessage = ({ data }: any) => {
-        if (!data || !data.__test_runner_content_script__) {
-          return;
-        }
-
-        this.contentWindow?.removeEventListener('message', handleMessage);
-        resolve();
-      };
-
-      this.contentWindow?.addEventListener('message', handleMessage);
-
-      const script = document.createElement('script');
-
-      script.src = 'http://localhost:7357/content_script.js';
-      body.appendChild(script);
-    });
+    return this.contentWindow?.localStorage.setItem(key, value);
   }
 
   async navigate(url: string) {
@@ -111,7 +47,6 @@ export class IFrame {
         this.element.onload = null;
 
         if (this.body) {
-          await this.injectContentScript();
           queries = getQueriesForElement(this.body);
         }
 
