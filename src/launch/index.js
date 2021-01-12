6dc3e97d20dc00c24a86b9c4448a7706e20d69c4
Nils <nils@epita.fr>
2021-01-12 22:21:57 +0100

    allow to specify the screenshots directory

--- src/launch/index.js
+++ src/launch/index.js
@@ -1,19 +1,34 @@
 const fs = require('fs').promises;
+const path = require('path');
 
 const { browser } = require('./browser');
 const setupMocha = require('./mocha');
 
+const arg = name => {
+  const idx = process.argv.indexOf(name);
+
+  if (idx > 0) {
+    return process.argv[idx + 1] || true;
+  }
+};
+
 const launch = async () => {
+  const screenshots = arg('--screenshots');
+
   const { runner, handleMochaLifecycle } = setupMocha();
   const { startEventsLoop, stopEventsLoop, takeScreenshot } = await browser();
 
   runner.on('end', stopEventsLoop);
   // process.on('SIGINT', stopEventsLoop);
 
-  runner.on('fail', async () => {
-    const screenshot = await takeScreenshot();
-    await fs.writeFile('/tmp/screenshot.png', Buffer.from(screenshot, 'base64'));
-  });
+  if (screenshots) {
+    runner.on('fail', async function (test) {
+      const screenshot = await takeScreenshot();
+      const filePath = path.join(screenshots, test.titlePath().join('_') + '.png');
+
+      await fs.writeFile(filePath, Buffer.from(screenshot, 'base64'));
+    });
+  }
 
   await startEventsLoop(handleMochaLifecycle);
 
